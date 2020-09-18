85b8d2f75a583e40be0c161256b2a62b1c7d175b
Harsh Raj <rafftar98@gmail.com>
2020-09-18 21:57:45 +0530

    bug fix

---
+++ Quizes.py
@@ -0,0 +1,22 @@
+# Quiz 1 : Create a program that takes two numbers as input from the user and then prints the sum of these numbers.
+# Solution in #4.
+
+
+# Quiz 2 : Create a program that takes a test for Driving Lincence.
+# The participant should be above 18 and Below 75 to get the Licence if he is still 18 then ask him to give a physical Driving test.
+# Solution in #9.
+
+
+# Quiz 3 : Create a program which filters a list containing varios Datatypes and only prints Integers which are greater than 6.
+# By using For Loop.
+# Solution in #10.
+
+
+# Quiz 4 : Create a program which takes Input from user and asks to enter a Number greater than 100.
+# Untill the user doesn't enter a number greater than 100 program continues.
+# Solution in #13. 
+
+
+# Quiz 5 : Create a program which takes Input from user and asks for to enter A number then create a function that 
+# Generates Fibunacci sequence and give the Fibunacci for the the numbers which was taken from user earlier.
+# Solution in #23.
