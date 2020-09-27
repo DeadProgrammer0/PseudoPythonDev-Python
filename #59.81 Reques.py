42e8b0ba2d75f56a3f0584aa1250d52a83ba8004
Harsh Raj <rafftar98@gmail.com>
2020-09-27 21:14:00 +0530

    Hid Credentials

--- #59.81 Reques.py
+++ #59.81 Reques.py
@@ -1,52 +1,30 @@
-# import requests
-# from bs4 import BeautifulSoup as bs
-
-# # spotify = requests.get('https://developer.spotify.com/')
-# # print(spotify.status_code)
-
-# # url = 'https://accounts.spotify.com/en/login?continue=https:%2F%2Fopen.spotify.com%2F'
-# # data = {
-# #     'username':'rafftar98@gmail.com',
-# #     'password':'7677730199'
-# #     }
-
-# # test = requests.post(url=url,data=data)
-# # print(test.status_code)
-
-# # #TODO : Make program using any API key.
-
-# url = 'https://mbasic.facebook.com/login/'
-# data = {
-#     'email':'rafftar98@gmail.com'
-#     ,'pass':'Jaihinddoston'
-# }
-
 import requests
-
-# payload = {
-#     'page':2,
-#     'count':25
-# }
-# ge = requests.get('http://httpbin.org/get', params=payload)
-
-# print(ge.url)
-
-# data = {
-#     'usename':'Max',
-#     'pass':'testing'
-# }
-# po = requests.post('http://httpbin.org/post', data=data)
-
-# r_dict = po.json()
-# print(r_dict['form'])
-
-# auth = ('max','testing')
-# au = requests.get('http://httpbin.org/basic-auth/cord/test', auth=('cord','test'))
-
-# print(au)
+from bs4 import BeautifulSoup as bs
 
 
-import os 
+# TODO : Make program using any API key.
+
+payload = {
+    'page':2,
+    'count':25
+}
+ge = requests.get('http://httpbin.org/get', params=payload)
+
+print(ge.url)
+
+data = {
+    'usename':'Max',
+    'pass':'testing'
+}
+po = requests.post('http://httpbin.org/post', data=data)
+
+r_dict = po.json()
+print(r_dict['form'])
+
+auth = ('max','testing')
+au = requests.get('http://httpbin.org/basic-auth/cord/test', auth=('cord','test'))
+
+print(au)
 
 
 
